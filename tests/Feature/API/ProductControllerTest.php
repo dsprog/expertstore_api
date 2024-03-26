18d76a2a1af0d12d9b3daf5a6cc5456fe726abcb
Daniel Henrique dos Santos Oliveira <dsprog@gmail.com>
2024-03-26 15:49:35 -0300

    testes produtos

--- tests/Feature/API/ProductControllerTest.php
+++ tests/Feature/API/ProductControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\API;
 
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Testing\Fluent\AssertableJson;
@@ -35,5 +36,53 @@ class ProductControllerTest extends TestCase
 
             $json->count('data', 3)->etc();
         });
+    }
+
+    public function test_should_product_get_endpoint_returns_a_single_product()
+    {
+        Product::factory(1)->create();
+
+        $response = $this->getJson('/api/products/4');
+
+        //$response->dd();
+        $response
+        //->dump()
+        ->assertStatus(200);
+        $response->assertJson(function(AssertableJson $json){
+
+            $json->has('data');
+
+            $json->hasAll(['data.name', 'data.price', 'data.price_float']);
+
+            $json->whereAllType([
+                'data.name' => 'string',
+                'data.price' => 'integer',
+                'data.price_float' => 'double'
+            ]);
+        });
+    }
+
+    public function test_should_product_post_endpoint_throw_an_unauthorized_status()
+    {
+        $response = $this->postJson('/api/products', []);
+        $response->assertUnauthorized();
+    }
+
+    public function test_should_product_post_endpoint_create_a_new_product()
+    {
+
+        $token = User::factory()->create();
+        $token = $token->createToken('default')->plainTextToken;
+
+        $headers = [
+            'Authorization' => 'Bearer ' . $token
+        ];
+
+        $product = [
+            'name' => 'produto teste 1',
+            'price' => 1200
+        ];
+        $response = $this->postJson('/api/products/', $product, $headers);
+        $response->assertCreated();
     }
 }
